92dc6ca67bb45dc5fad223c11438486998603b41
Tawfek Daghistani <tawfekov@gmail.com>
2015-06-30 04:00:33 +0300

    small fixes on adminhtml + fix calculation

---
+++ app/code/local/Yellow/Bitcoin/Block/Adminhtml/Sales/Order/Total/Fee.php
@@ -0,0 +1,10 @@
+<?php
+
+class Yellow_Bitcoin_Block_Adminhtml_Sales_Order_Total_Fee extends Mage_Adminhtml_Block_Sales_Order_Create_Totals_Default
+{
+    /**
+     * Use your own template if necessary
+     * See "sales/order/create/totals/default.phtml" for model
+     */
+    //protected $_template = 'bitcoin/sales/order/total/fee.phtml';
+}
